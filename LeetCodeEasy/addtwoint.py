df03939a5844472eeaeae311ce5d029d03b50710
FreddieM <104393395+fmontman90@users.noreply.github.com>
2023-11-13 23:53:24 -0500

    leet code folder
    created leetcode folder for work done

---
+++ LeetCodeEasy/addtwoint.py
@@ -0,0 +1,11 @@
+# This will print the sum of two integers
+
+def sum(num1: int, num2: int) -> int:
+    
+    return num1 + num2
+   
+print(sum(5,12)) # should be 17
+
+print(sum(-10,4)) # should be -6
+
+#Leet code easy Question
